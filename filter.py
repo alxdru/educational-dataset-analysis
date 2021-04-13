1df4c4d905a02990866d45428c886477e947b80a
alexandru.radu@sap.com <alexandru.radu@sap.com>
2021-04-13 21:00:23 +0300

    comparison between teacher population on all levels, per countries

--- filter.py
+++ filter.py
@@ -18,7 +18,8 @@ original['sum']=original.sum(axis=1)
 # The rows that their sum is not 0 remain
 total=original[original['sum']!=0]
 
+countries = ['East Asia & Pacific', 'China', 'Hong Kong', 'Japan', 'Macau', 'Mongolia', 'Korea, Dem. People’s Rep.', 'North Korea', 'South Korea', 'Taiwan', 'Brunei', 'Cambodia', 'Indonesia', 'Malaysia', 'Myanmar', 'Philippines', 'Singapore', 'Thailand', 'Vietnam']
 # East Asia & Pacific filter
-total = total[total['CountryName']=='East Asia & Pacific']
+total = total[total['CountryName'].isin(countries)]
 
 total.to_csv('total.csv')
