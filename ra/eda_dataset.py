d98774fa52baba79ed06240ed742fcbfea246140
alexandru.radu@sap.com <alexandru.radu@sap.com>
2021-04-30 23:04:10 +0300

    applied extrapolation:

--- ra/eda_dataset.py
+++ ra/eda_dataset.py
@@ -22,7 +22,6 @@ population_by_education_tertiary.head()
 enrolment_tertiary = total[total['IndicatorName']=='Enrolment in tertiary education, all programmes, both sexes (number)']
 enrolment_tertiary.head()
 
-
 # Pivot table for teachers on the 3 levels of education
 teachers_all_levels = total[total['IndicatorName'].str.contains('Teachers in') & total['IndicatorName'].str.contains('both sexes')]
 teachers_all_levels_pt=pd.pivot_table(teachers_all_levels, index='CountryCode', columns='IndicatorName', values=['2010'], dropna=True)
@@ -34,23 +33,24 @@ teachers_all_levels_pt=teachers_all_levels_pt.drop('sum',axis=1)
 
 # Bar plots for all education levels per each country
 teachers_all_levels_pt=teachers_all_levels_pt[::-1]
-pal=sns.cubehelix_palette(8)
-teachers_graph=teachers_all_levels_pt.plot(kind='barh', stacked=True, figsize=(20,36), color=pal, title='Teachers by level of education: Lower Secondary (both), Pre-Primary (both), Primary (both), Secondary (both), Secondary General (both), Secondary Vocational (both), tertiary ISCED 5 (both), tertiary programmes (both), upper education (both)', legend=False, xlim=(0,10000000))
+pal=sns.diverging_palette(220, 20, as_cmap=True)
+teachers_graph=teachers_all_levels_pt.plot(kind='barh', stacked=True, figsize=(20,36), colormap=pal, title='Population of Teachers by level of education', legend=True, xlim=(0,10000000))
 teachers_graph
 
-# Pivot table for Populations of the official ages per levels of education
-population_all_levels = total[total['IndicatorName'].str.contains('Population of the official age for') & total['IndicatorName'].str.contains('both sexes')]
-population_all_levels_pt = pd.pivot_table(population_all_levels, index='CountryCode', columns='IndicatorName', values=['2010'], dropna=True)
+# # Pivot table for Populations of the official ages per levels of education
+# population_all_levels = total[total['IndicatorName'].str.contains('Population of the official age for') & total['IndicatorName'].str.contains('both sexes')]
+# population_all_levels_pt = pd.pivot_table(population_all_levels, index='CountryCode', columns='IndicatorName', values=['2010'], dropna=True)
 
-population_all_levels_pt['sum'] = population_all_levels_pt.sum(axis=1)
-population_all_levels_pt = population_all_levels_pt[population_all_levels_pt['sum']!=0]
+# population_all_levels_pt['sum'] = population_all_levels_pt.sum(axis=1)
+# population_all_levels_pt = population_all_levels_pt[population_all_levels_pt['sum']!=0]
 
-population_all_levels_pt = population_all_levels_pt.drop('sum', axis=1)
+# population_all_levels_pt = population_all_levels_pt.drop('sum', axis=1)
 
-population_all_levels_pt=population_all_levels_pt[::-1]
-pal=sns.cubehelix_palette(8, start=2, rot=0, dark=0, light=.95)
-population_graph=population_all_levels_pt.plot(kind='barh', stacked=True, figsize=(20,36), color=pal, title='Population of the official age for: Lower Secondary, Post-secondary non-tertiary, Pre-Primary, Primary, Secondary, Tertiary, Last grade of primary education, Upper secondary education', legend=False, xlim=(0,10000000))
-population_graph
+# # Bar plots for all education levels per each country
+# population_all_levels_pt=population_all_levels_pt[::-1]
+# pal=sns.cubehelix_palette(8, start=2, rot=0, dark=0, light=.95)
+# population_graph=population_all_levels_pt.plot(kind='barh', stacked=True, figsize=(20,36), color=pal, title='Population of the official age for: Lower Secondary, Post-secondary non-tertiary, Pre-Primary, Primary, Secondary, Tertiary, Last grade of primary education, Upper secondary education', legend=False, xlim=(0,10000000))
+# population_graph
 
 # Evolutionary graph for GDP and GNI
 import matplotlib.style as style
@@ -59,56 +59,63 @@ style.use('default')
 years = list(map(lambda y: str(y), list(range(1996,2015))))
 
 indicators = [
-    "GDP per capita (current US$)",
-    "GDP per capita, PPP (current international $)", 
-    "GNI (current US$)", 
-    "GNI, PPP (current international $)",
-    "Literacy rate, population 25-64 years, both sexes (%)",
     "Adult literacy rate, population 15+ years, both sexes (%)",
     "Youth literacy rate, population 15-24 years, both sexes (%)",
     "Unemployment, total (% of total labor force)",
     "Internet users (per 100 people)",
-    "Personal computers (per 100 people)",
-    "Pupil-teacher ratio in tertiary education (headcount basis)",
-    "School life expectancy, tertiary, both sexes (years)",
-    "Labor force with advanced education (% of total)"
-    ]
+    "Personal computers (per 100 people)"
+]
 
 countries_macro = ['World', 'East Asia & Pacific']
 
+macro_exceptions = [
+    "Adult literacy rate, population 15+ years, both sexes (%)",
+    "Youth literacy rate, population 15-24 years, both sexes (%)"
+]
 ########## Plotting list of indicators
 
 for indicator in indicators:
+    if indicator in macro_exceptions:
+        world_asia = total[total['IndicatorName'].isin([indicator])]
 
-    countries = total[total['IndicatorName'].isin([indicator]) & ~total['CountryName'].isin(countries_macro)]
-    top_5_countries = countries.nlargest(5, 'sum')
-    bottom_5_countries = countries.nsmallest(5, 'sum')
+        fig, ax = plt.subplots()
+        world_asia = world_asia[world_asia['CountryName'].isin(countries_macro)]
+        for c in ['CountryName', 'CountryCode', 'IndicatorName', 'Unnamed: 0']:
+            del world_asia[c]
+        world_asia = world_asia[years]
+        world_asia = world_asia.T
+        world_asia.plot(lw=2, ax=ax, colormap='jet', marker='.', markersize=10, title="{0} respective to top 5 countries".format(indicator))
+        ax.legend(countries_macro)
+    else:
+        countries = total[total['IndicatorName'].isin([indicator]) & ~total['CountryName'].isin(countries_macro)]
+        top_5_countries = countries.nlargest(5, 'sum')
+        bottom_5_countries = countries.nsmallest(5, 'sum')
 
-    ## Top 5
-    world_vs_asia = total[total['IndicatorName'].isin([indicator])]
+        ## Top 5
+        world_vs_asia = total[total['IndicatorName'].isin([indicator])]
 
-    world_region_and_highest = countries_macro + top_5_countries['CountryName'].tolist()
-    fig, ax = plt.subplots()
-    world_vs_asia = world_vs_asia[world_vs_asia['CountryName'].isin(world_region_and_highest)]
-    for c in ['CountryName', 'CountryCode', 'IndicatorName', 'Unnamed: 0']:
-        del world_vs_asia[c]
-    world_vs_asia = world_vs_asia[years]
-    world_vs_asia = world_vs_asia.T
-    world_vs_asia.plot(lw=2, ax=ax, colormap='jet', marker='.', markersize=10, title="{0} with top 5 countries".format(indicator))
-    ax.legend(world_region_and_highest)
+        world_region_and_highest = countries_macro + top_5_countries['CountryName'].tolist()
+        fig, ax = plt.subplots()
+        world_vs_asia = world_vs_asia[world_vs_asia['CountryName'].isin(world_region_and_highest)]
+        for c in ['CountryName', 'CountryCode', 'IndicatorName', 'Unnamed: 0']:
+            del world_vs_asia[c]
+        world_vs_asia = world_vs_asia[years]
+        world_vs_asia = world_vs_asia.T
+        world_vs_asia.plot(lw=2, ax=ax, colormap='jet', marker='.', markersize=10, title="{0} respective to top 5 countries".format(indicator))
+        ax.legend(world_region_and_highest)
 
-    ## Bottom 5
-    world_vs_asia = total[total['IndicatorName'].isin([indicator])]
+        ## Bottom 5
+        world_vs_asia = total[total['IndicatorName'].isin([indicator])]
 
-    world_region_and_lowest = countries_macro + bottom_5_countries['CountryName'].tolist()
-    fig2, ax2 = plt.subplots()
-    world_vs_asia = world_vs_asia[world_vs_asia['CountryName'].isin(world_region_and_lowest)]
-    for c in ['CountryName', 'CountryCode', 'IndicatorName', 'Unnamed: 0']:
-        del world_vs_asia[c]
-    world_vs_asia = world_vs_asia[years]
-    world_vs_asia = world_vs_asia.T
-    world_vs_asia.plot(lw=2, ax=ax2, colormap='jet', marker='.', markersize=10, title="{0} with bottom 5 countries".format(indicator))
-    ax2.legend(world_region_and_lowest)
+        world_region_and_lowest = countries_macro + bottom_5_countries['CountryName'].tolist()
+        fig2, ax2 = plt.subplots()
+        world_vs_asia = world_vs_asia[world_vs_asia['CountryName'].isin(world_region_and_lowest)]
+        for c in ['CountryName', 'CountryCode', 'IndicatorName', 'Unnamed: 0']:
+            del world_vs_asia[c]
+        world_vs_asia = world_vs_asia[years]
+        world_vs_asia = world_vs_asia.T
+        world_vs_asia.plot(lw=2, ax=ax2, colormap='jet', marker='.', markersize=10, title="{0} with bottom 5 countries".format(indicator))
+        ax2.legend(world_region_and_lowest)
 
 
 
--- ra/modeling.py
+++ ra/modeling.py
@@ -7,6 +7,7 @@ import seaborn as sns
 from scipy.stats.stats import pearsonr
 from statsmodels.tsa.arima.model import ARIMA
 from random import random
+import math
 
 # Read original file
 total=pd.read_csv('./total.csv')
@@ -18,19 +19,9 @@ years = list(map(lambda y: str(y), list(range(1977,2016))))
 all_years = []
 indicators = [
     "GDP per capita (current US$)",
-    # "GDP per capita, PPP (current international $)", 
-    # "GNI (current US$)", 
-    # "GNI, PPP (current international $)",
-    # "Literacy rate, population 25-64 years, both sexes (%)",
-    # "Adult literacy rate, population 15+ years, both sexes (%)",
-    # "Youth literacy rate, population 15-24 years, both sexes (%)",
     "Unemployment, total (% of total labor force)",
-    # "Internet users (per 100 people)",
-    # "Personal computers (per 100 people)",
-    # "Pupil-teacher ratio in tertiary education (headcount basis)",
-    # "School life expectancy, tertiary, both sexes (years)",
     "Labor force with advanced education (% of total)"
-    ]
+]
 
 countries_macro = ['World', 'East Asia & Pacific']
 
@@ -54,17 +45,28 @@ for indicator in indicators:
         data_country = data_country[years]
         data_country = data_country.T
         if not data_country.empty:
-
+            
             ## Forecasting additional 15 years as variable candidates for regression
             data_list = data_country.iloc[:,0].tolist()
             model = ARIMA(data_list, order=(1,1,1))
             model_fit = model.fit()
             data_predicted = model_fit.predict(start = len(data_list), end = len(data_list) + 14, typ='levels')
             
-            all_data = data_list + data_predicted.tolist()
+            extrapolated_value = math.nan
+            extrapolated_index = None
+            for index, value in enumerate(data_list):
+                if not math.isnan(value):
+                    extrapolated_value = value
+                    extrapolated_index = index
+
+            extrapolation = list(map(lambda y: extrapolated_value, list(range(0,extrapolated_index+1))))
+            del data_list[0:(extrapolated_index+1)]
+            all_data = extrapolation + data_list + data_predicted.tolist()
 
             df_forecast = pd.DataFrame([all_data], columns=all_years)
-
+            print("For Country: ", country, "For indicator: ",indicator)
+            with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+                display(" following dataframe: ", df_forecast)
             df_forecast['Country'] = country
             df_record = df_record.append(df_forecast, ignore_index=True)
 
